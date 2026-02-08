36a59a7ce91e32d30a1348e2a04eb7981586e4cc
zaidelayan77-star <zaidelayan77@gmail.com>
2026-02-08 11:04:58 +0300

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+  base: "/new-portfolio/",
+});
